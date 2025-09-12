603950a0e663bf2bb8cc55bb9f3529e4290c1c07
vansh-home <dadhichvansh46@gmail.com>
2025-09-12 20:18:58 +0530

    Create food partner db model

---
+++ server/src/models/foodPartner.model.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const foodPartnerSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const FoodPartner = mongoose.model('FoodPartner', foodPartnerSchema);
+export default FoodPartner;
