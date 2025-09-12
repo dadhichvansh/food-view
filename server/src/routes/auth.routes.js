79e8399f7b800552da6ecd5c5ba3b35aa3f1310f
vansh-home <dadhichvansh46@gmail.com>
2025-09-12 20:19:38 +0530

    Add food partner auth routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import {
+  loginFoodPartner,
   loginUser,
+  logoutFoodPartner,
   logoutUser,
+  registerFoodPartner,
   registerUser,
 } from '../controllers/auth.controller.js';
 
 // Router instance
 const router = express.Router();
 
-// Auth routes
+// User auth APIs
 router.post('/user/register', registerUser);
 router.post('/user/login', loginUser);
 router.get('/user/logout', logoutUser);
+
+// Food Partner auth APIs
+router.post('/food-partner/register', registerFoodPartner);
+router.post('/food-partner/login', loginFoodPartner);
+router.get('/food-partner/logout', logoutFoodPartner);
 
 export default router;
