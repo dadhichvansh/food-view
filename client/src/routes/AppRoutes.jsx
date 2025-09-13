6328605f0218538222ac91d800ae65cf76aea01e
vansh-home <dadhichvansh46@gmail.com>
2025-09-13 19:44:43 +0530

    Setup frontend files

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserRegister from '../pages/auth/UserRegister';
+import ChooseRegister from '../pages/auth/ChooseRegister';
+import UserLogin from '../pages/auth/UserLogin';
+import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister';
+import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin';
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<ChooseRegister />} />
+        <Route path="/user/register" element={<UserRegister />} />
+        <Route path="/user/login" element={<UserLogin />} />
+        <Route
+          path="/food-partner/register"
+          element={<FoodPartnerRegister />}
+        />
+        <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
