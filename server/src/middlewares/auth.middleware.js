0465291cf4285e5ce1b52a9d1d80a8d35a1bd3e3
vansh-home <dadhichvansh46@gmail.com>
2025-09-13 21:12:16 +0530

    Define user auth middleware

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import FoodPartner from '../models/foodPartner.model.js';
+import User from '../models/user.model.js';
 
 export async function authFoodPartnerMiddleware(req, res, next) {
   const token = req.cookies.token;
@@ -18,6 +19,30 @@ export async function authFoodPartnerMiddleware(req, res, next) {
     next();
   } catch (error) {
     console.error('Error in authFoodPartnerMiddleware():', error);
+    return res.status(401).json({
+      ok: false,
+      message: 'Unauthorized access',
+    });
+  }
+}
+
+export async function authUserMiddleware(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      message: 'Please login first',
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error('Error in authUserMiddleware():', error);
     return res.status(401).json({
       ok: false,
       message: 'Unauthorized access',
